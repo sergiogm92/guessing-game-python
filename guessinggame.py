0f251933ddcad1d31f333946c45f16201274d649
sergiogm92 <sergiogarcia_10@hotmail.com>
2020-09-27 23:39:04 +0200

    first steps

--- guessinggame.py
+++ guessinggame.py
@@ -4,5 +4,24 @@ print("----------------------------")
 print("     M&M guessing game!")
 print("----------------------------")
 
-mm_count = random.randint(1, 100)
+print('Guess the number of M&Ms and you get lunch on the house')
+print()
 
+mm_count = random.randint(1, 100)
+attempt_limit = 5
+attemps = 0
+
+while attemps < attempt_limit:
+    guess_text = input('How manu M&Ms are in the jar? ')
+    guess = int(guess_text)
+    attemps += 1
+
+    if mm_count == guess:
+        print(f'You got a free lunch! It was {guess}.')
+        break
+    elif guess < mm_count:
+        print("Sorry, that's too low!")
+    else:
+        print("That's too high")
+
+print(f"Bye, you're done in {attemps}")
